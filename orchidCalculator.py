c99198992663107a61695242e1b5acb3885b4ae5
Edward <64510369+edwardough@users.noreply.github.com>
2021-03-01 15:53:39 +1100

    Update orchidCalculator.py
    Finished to do list. Still not thoroughly tested

--- orchidCalculator.py
+++ orchidCalculator.py
@@ -1,8 +1,14 @@
+import math
+import logging
+logging.basicConfig(filename='myProgramLog.txt', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s -  %(message)s')
+# logging.disable() 
+
 priceList = {
-    'vandas' : 7.35,
-    'miltonia' : 9.80,
-    'cattleya' : 10.00
+    'V' : 7.35, # vandas
+    'M' : 9.80, # miltonia
+    'C' : 10.00 # cattleya
 }
+
 while True:
     member = input("Are you a member? [Y] or [N] > " )
     if member.upper() == 'Y':
@@ -13,12 +19,37 @@ while True:
         break
     else:
         print("Please enter Y or N")
+
 while True:
     orchid = input("Which Orchid would you like? [V]andas, [M]iltonia, or [C]attleya > ")
     if orchid.upper() not in ['V','M','C']:
         print("Please enter V, M or C")
     else:
         break
-print("Member status:",member)
-print("Chosen orchid:",orchid)
-# TO DO Quantity input, finalPrice calculation
+
+logging.debug("Member status > " + str(member))
+logging.debug("Chosen orchid > " + str(orchid))
+
+while True:
+    try:
+        quantity = int(input("How many do you want? > "))
+        # if quantity not in range(0,100):
+        if quantity < 0 or quantity > 100:
+            print("Please enter a value between 1 and 99")
+        else:
+            price = quantity * priceList[orchid]
+            logging.debug("Pre discount price > " + str(price))
+            if member == True:
+                # apply member discount
+                price *= 0.90
+            if quantity >= 5:
+                # apply quantity discount
+                price *= 0.95
+            price = round(price,2)
+            break
+    except ValueError:
+        print("Enter a number only please.")
+
+print("Your final price is > $",format(price,'.2f'))
+
+#TO DO - Proper testing
